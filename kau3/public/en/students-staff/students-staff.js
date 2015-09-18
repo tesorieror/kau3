572b377009043ec2f597b2260a408b6c3bd24865
ricardo.tesoriero <ricardo.tesoriero@localhost>
2015-09-18 11:25:23 +0200

    Staff fixed!

--- kau3/public/en/students-staff/students-staff.js
+++ kau3/public/en/students-staff/students-staff.js
@@ -1,7 +1,25 @@
-app.controller("StudentsStaffCtrl", function($scope, $log, $location) {
+app.controller("StudentsStaffCtrl", function($scope, $log, $location, ModelFactory) {
 	$log.info("University Staff Controller loaded!");
 	$scope.activePath = $location.path();
-	$scope.tagCategoryNames = [ 'YR', 'IT', 'IN', 'UP', 'NA', 'GE' ];
+	var tagCategoryNames = [ 'YR', 'IT', 'IN', 'UP', 'NA', 'GE' ];
+	
+	
+	$scope.tagCategories = [];
+	/**
+	 * Load Categories
+	 */
+	ModelFactory.getTagCategoriesForNames(tagCategoryNames)//
+	.then(function(data) {
+		// $log.log("data.data", data.data);
+		var patchedTagCategories = _.sortBy(data.data, function(cat) {
+			return tagCategoryNames.indexOf(cat.name);
+		});
+		$log.log('Patched Categories', patchedTagCategories);
+		$scope.tagCategories = patchedTagCategories;
+	});
+	
+	
+	
 	$scope.title = "University Staff";
 	$log.log("activePath", $scope.activePath);
 
