f570f079e049dce3465151ff8b0c514cc7be20f2
ricardo.tesoriero <ricardo.tesoriero@localhost>
2015-09-18 12:23:57 +0200

    Undergraduate freshmen students FIXED!

--- kau3/public/en/students-freshmen-undergraduate/students-freshmen-undergraduate.js
+++ kau3/public/en/students-freshmen-undergraduate/students-freshmen-undergraduate.js
@@ -1,10 +1,38 @@
-app.controller("StudentsFreshmenUndergraduateCtrl",
-		function($scope, $log, $location) {
-			$log.info("Freshmen Undergesuate Students Controller loaded!");
-			$scope.activePath = $location.path();
-			$scope.tagCategoryNames = [ 'YR', 'SS', 'DS', 'SY', 'IN', 'IT', 'NA',
-					'GE' ];
-			$scope.title = "Freshmen Undergraduate Students";
-			$log.log("activePath", $scope.activePath);
+app.controller("StudentsFreshmenUndergraduateCtrl", function($scope, $log, $location, ModelFactory) {
+	$log.info("Freshmen Undergesuate Students Controller loaded!");
+	$scope.activePath = $location.path();
+	var tagCategoryNames = [ 'YR', 'SS', 'DS', 'SY', 'IN', 'IT', 'NA', 'GE' ];
 
+	$scope.tagCategories = [];
+	/**
+	 * Load Categories
+	 */
+	ModelFactory.getTagCategoriesForNames(tagCategoryNames)//
+	.then(function(data) {
+		// $log.log("data.data", data.data);
+		var patchedTagCategories = _.sortBy(data.data, function(cat) {
+			return tagCategoryNames.indexOf(cat.name);
 		});
+
+		var ssCat = _.detect(patchedTagCategories, function(cat) {
+			return cat.name == "SS";
+		});
+		ssCat._tags = _.filter(ssCat._tags, function(t) {
+			return t.name == "FR";
+		});
+
+		var dsCat = _.detect(patchedTagCategories, function(cat) {
+			return cat.name == "DS";
+		});
+		dsCat._tags = _.filter(dsCat._tags, function(t) {
+			return t.name == "UG";
+		});
+
+		$log.log('Patched Categories', patchedTagCategories);
+		$scope.tagCategories = patchedTagCategories;
+	});
+
+	$scope.title = "Freshmen Undergraduate Students";
+	$log.log("activePath", $scope.activePath);
+
+});
