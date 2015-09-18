e6b7101748eecec3dd5a3d8e6d179e0063367afe
ricardo.tesoriero <ricardo.tesoriero@localhost>
2015-09-18 11:12:07 +0200

    Community Service Fixed!

--- kau3/public/en/students-community-service/students-community-service.js
+++ kau3/public/en/students-community-service/students-community-service.js
@@ -1,7 +1,22 @@
-app.controller("StudentsCommunityServiceCtrl", function($scope, $log, $location) {
+app.controller("StudentsCommunityServiceCtrl", function($scope, $log, $location, ModelFactory) {
 	$log.info("Universities By Community Services Controller loaded!");
 	$scope.activePath = $location.path();
-	$scope.tagCategoryNames = [ 'YR', 'CCS', 'PE', 'GE' ];
+	var tagCategoryNames = [ 'YR', 'IT', 'IN', 'CCS', 'PE', 'GE' ];
+
+	$scope.tagCategories = [];
+	/**
+	 * Load Categories
+	 */
+	ModelFactory.getTagCategoriesForNames(tagCategoryNames)//
+	.then(function(data) {
+		// $log.log("data.data", data.data);
+		var patchedTagCategories = _.sortBy(data.data, function(cat) {
+			return tagCategoryNames.indexOf(cat.name);
+		});
+		$log.log('Patched Categories', patchedTagCategories);
+		$scope.tagCategories = patchedTagCategories;
+	});
+
 	$scope.title = "Universities by Community Services";
 	$log.log("activePath", $scope.activePath);
 });
